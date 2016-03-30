79ee29300aa75a9cb7b8a86802f6db6bbd57ce65
Paul Gomersbach <p.gomersbach@rely.nl>
2016-03-30 12:52:54 +0200

    add spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -13,10 +13,35 @@ describe 'role_ids' do
         context "role_ids class with monitor_interface set to eth0" do
           let(:params) { {:monitor_interface => "eth0"} }
 
+          # should compile
           it { is_expected.to compile.with_all_deps }
+
+          # contain classes
           it { is_expected.to contain_class('role_ids') }
           it { is_expected.to contain_class('suricata').that_comes_before('scirius')  }
           it { is_expected.to contain_class('scirius') }
+
+          # contain packages
+          it { is_expected.to contain_package('suricata') }
+          it { is_expected.to contain_package('ethtool') }
+          it { is_expected.to contain_package('libhtp1') }
+          it { is_expected.to contain_package('python-pyinotify') }
+          it { is_expected.to contain_package('python-software-properties') }
+          it { is_expected.to contain_package('software-properties-common') }
+          it { is_expected.to contain_package('git') }
+          it { is_expected.to contain_package('expect') }
+          it { is_expected.to contain_package('python-dev') }
+          it { is_expected.to contain_package('python-django') }
+          it { is_expected.to contain_package('python-docutils') }
+          it { is_expected.to contain_package('python-imaging') }
+          it { is_expected.to contain_package('python-markdown') }
+          it { is_expected.to contain_package('python-pip') }
+          it { is_expected.to contain_package('python-pyinotify') }
+          it { is_expected.to contain_package('python-pythonmagick') }
+          it { is_expected.to contain_package('python-textile') }
+          it { is_expected.to contain_package('south') }
+
+          # contain logrotate rule
           it { is_expected.to contain_logrotate__rule('suricata') }
 
         end
